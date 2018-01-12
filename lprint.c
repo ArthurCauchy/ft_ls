ae777ccfccdd87b849acb59e5a5f65b376ea40cb
ArthurCauchy <arthur.cauchy@free.fr>
2018-01-12 12:18:48 +0100

    affichage option -l decent

--- lprint.c
+++ lprint.c
@@ -6,7 +6,7 @@
 /*   By: acauchy <acauchy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/08 10:55:47 by acauchy           #+#    #+#             */
-/*   Updated: 2018/01/11 18:33:18 by arthur           ###   ########.fr       */
+/*   Updated: 2018/01/12 10:55:46 by arthur           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,12 +26,26 @@ void		lprint_delete(void *lprint, size_t size)
 	free(lprint);
 }
 
+static char	*major_minor_to_str(t_fileinfo *fileinfo)
+{
+	char	*maj;
+	char	*min;
+	char	*tmp;
+	char	*final;
+
+	tmp = ft_itoa(fileinfo->major);
+	maj = ft_strfill_right(tmp, 3, ' ');
+	free(tmp);
+	tmp = ft_itoa(fileinfo->minor);
+	min = ft_strfill_right(tmp, 3, ' ');
+	free(tmp);
+	final = ft_strjoin_free(maj, ft_strjoin_free(ft_strdup(", "), min));
+	return (final);
+}
+
 t_lprint	*lprint_new(t_fileinfo *fileinfo, int short_name)
 {
 	t_lprint	*new;
-	char		*major;
-	char		*tmp;
-	char		*minor;
 	
 	new = (t_lprint*)malloc(sizeof(t_lprint));
 	new->mode = ft_strdup(fileinfo->mode);
@@ -41,16 +55,7 @@ t_lprint	*lprint_new(t_fileinfo *fileinfo, int short_name)
 	new->gid = (getgrgid(fileinfo->gid)) ?
 		ft_strdup(getgrgid(fileinfo->gid)->gr_name) : ft_itoa(fileinfo->gid);
 	if (fileinfo->mode[0] == 'b' || fileinfo->mode[0] == 'c')
-	{
-		tmp = ft_itoa(fileinfo->major);
-		major = ft_strfill_right(tmp, 3, ' ');
-		free(tmp);
-		tmp = ft_itoa(fileinfo->minor);
-		minor = ft_strfill_right(tmp, 3, ' ');
-		free(tmp);
-		new->size = ft_strjoin_free(major,
-				ft_strjoin_free(ft_strdup(", "), minor));
-	}
+		new->size = major_minor_to_str(fileinfo);
 	else
 		new->size = ft_lltoa(fileinfo->size);
 	new->date = time_to_str(fileinfo->mtime);
@@ -61,11 +66,17 @@ t_lprint	*lprint_new(t_fileinfo *fileinfo, int short_name)
 void		lprint_print(t_list *elem)
 {
 	ft_putstr(((t_lprint*)elem->content)->mode);
+	ft_putchar(' ');
 	ft_putstr(((t_lprint*)elem->content)->nlink);
+	ft_putchar(' ');
 	ft_putstr(((t_lprint*)elem->content)->uid);
+	ft_putchar(' ');
 	ft_putstr(((t_lprint*)elem->content)->gid);
+	ft_putchar(' ');
 	ft_putstr(((t_lprint*)elem->content)->size);
+	ft_putchar(' ');
 	ft_putstr(((t_lprint*)elem->content)->date);
+	ft_putchar(' ');
 	ft_putstr(((t_lprint*)elem->content)->filename);
 	ft_putchar('\n');
 }
--- output_l.c
+++ output_l.c
@@ -6,11 +6,56 @@
 /*   By: acauchy <acauchy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/08 10:55:47 by acauchy           #+#    #+#             */
-/*   Updated: 2018/01/11 18:33:40 by arthur           ###   ########.fr       */
+/*   Updated: 2018/01/12 12:07:28 by arthur           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
+
+static void	read_col_sizes(int *col_sizes, t_lprint *lp)
+{
+	int	len_nlink;
+	int	len_uid;
+	int	len_gid;
+	int	len_size;
+
+	len_nlink = ft_strlen(lp->nlink);
+	len_uid = ft_strlen(lp->uid);
+	len_gid = ft_strlen(lp->gid);
+	len_size = ft_strlen(lp->size);
+	if (len_nlink > col_sizes[0])
+		col_sizes[0] = len_nlink;
+	if (len_uid > col_sizes[1])
+		col_sizes[1] = len_uid;
+	if (len_gid > col_sizes[2])
+		col_sizes[2] = len_gid;
+	if (len_size > col_sizes[3])
+		col_sizes[3] = len_size;
+}
+
+static void	apply_col_sizes(int *col_sizes, t_list *lplist)
+{
+	t_lprint	*lp;
+	char		*tmp;
+
+	while (lplist)
+	{
+		lp = (t_lprint*)lplist->content;
+		tmp = ft_strfill_right(lp->nlink, col_sizes[0], ' ');
+		free(lp->nlink);
+		lp->nlink = tmp;
+		tmp = ft_strfill_right(lp->uid, col_sizes[1], ' ');
+		free(lp->uid);
+		lp->uid = tmp;
+		tmp = ft_strfill_right(lp->gid, col_sizes[2], ' ');
+		free(lp->gid);
+		lp->gid = tmp;
+		tmp = ft_strfill_right(lp->size, col_sizes[3], ' ');
+		free(lp->size);
+		lp->size = tmp;
+		lplist = lplist->next;
+	}
+}
 
 /*
 ** params :
@@ -26,19 +71,17 @@ void	print_l_line(t_fileinfo *fileinfo, int short_name, int pos)
 	static t_list	*lprintlist = NULL;
 	t_lprint		*lp_new;
 
+	if (!col_sizes)
+		col_sizes = ft_memalloc(4 * sizeof(int));
+	lp_new = lprint_new(fileinfo, short_name);
+	ft_lstpushback(&lprintlist, ft_lstnew((void*)lp_new, sizeof(t_lprint)));
+	read_col_sizes(col_sizes, lp_new);
 	if (pos == 1 || pos == 2)
 	{
+		apply_col_sizes(col_sizes, lprintlist);
 		ft_lstiter(lprintlist, &lprint_print);
 		ft_lstdel(&lprintlist, &lprint_delete);
 		free(col_sizes);
 		col_sizes = NULL;
-	}
-	else
-	{
-		if (!col_sizes)
-			col_sizes = ft_memalloc(4 * sizeof(int));
-		lp_new = lprint_new(fileinfo, short_name);
-		ft_lstpushback(&lprintlist, ft_lstnew((void*)lp_new, sizeof(t_lprint)));
-		//update_cols(col_sizes, lp_new);
 	}
 }
